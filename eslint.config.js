4426335bfc2d080c0c893bb1b5ca268b4c7eafcd
mahamudul hasan seber <sebermh@gmail.com>
2025-08-16 15:55:00 +0600

    DaisyUi  SetUp

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,7 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      node: true,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-4xl bg-black">MH Seber</h1>
+        <h1 className="text-4xl ">MH Seber</h1>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
       </div>
     </>
   );
